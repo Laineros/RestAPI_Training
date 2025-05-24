7eb70297d2495d8652cf4397bb2d4a467e1ec8b5
leo <LevchenkoLM23@st.ithub.ru>
2025-05-24 23:36:33 +0300

    GetPost

--- src/main/java/com/laineros/trainingRestAPI/controller/MainController.java
+++ src/main/java/com/laineros/trainingRestAPI/controller/MainController.java
@@ -1,13 +1,46 @@
 package com.laineros.trainingRestAPI.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laineros.trainingRestAPI.entity.Cat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping("/api/main")
     public String mainListener() {
         return "Whassup";
+    }
+
+    @GetMapping("api/cat")
+    public String getCat() {
+        Cat cat = new Cat("Murzik", 3, 8);
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(cat);
+        } catch (JsonProcessingException e) {
+            System.out.println("Error with cat");
+        }
+        return json;
+    }
+
+    @PostMapping("api/special")
+    public String postSpecialCat(@RequestParam String name) {
+        Cat cat = new Cat(name, 3, 8);
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(cat);
+        } catch (JsonProcessingException e) {
+            System.out.println("Error with cat");
+        }
+        return json;
     }
 
 }
